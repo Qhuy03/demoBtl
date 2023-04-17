9bd7854ed822d12ea34d96d0c8b0e82b07a6e5c1
Qhuy03 <huyntq2003@gmail.com>
2023-04-17 21:56:29 +0700

    huy update

--- assets/js/showDataInforFirm.js
+++ assets/js/showDataInforFirm.js
@@ -3,8 +3,12 @@ let idValue;
 // get film id from url
 let queryString = window.location.search;
 const params = new URLSearchParams(queryString);
-const filmId = params.get('filmId');
-
+let filmId  = 1;
+if(params.get('filmId')){
+    filmId = params.get('filmId');
+}
+ 
+let valuePresent;
 
 // get film from localstorage by id
 dataFilmLocalStorage = localStorage.getItem('film'); 
@@ -15,8 +19,9 @@ for (const itemt of dataFilm) {
     if(itemt.id == filmId) {
         valuePresent = itemt;
         console.log(valuePresent)
-        break;
+       
     }
+    
 }
 
 // clickSHowFirm
@@ -42,11 +47,11 @@ let showMoreDetailFirm = document.querySelector(".container-more-detail-firm");
 
 // infomationFirm 
 
-
+console.log(valuePresent);
 showMoreDetailFirm.innerHTML = `
 <div class="left-more-detail-firm">
 <div class="itemt-img-firm">
-    <img src="${valuePresent.anh_phim}" alt="">
+    <img src="${valuePresent['anh_phim']}" alt="">
     <div class="container-buton-itemt-img-firm">
         <div class="buton-itemt-img-firm"><a href="#">Tải phim</a></div>
         <div class="buton-itemt-img-firm"><a href="#">Xem phim</a></div>
@@ -57,7 +62,7 @@ showMoreDetailFirm.innerHTML = `
 <div class="right-more-detail-firm">
 <div class="container-detail-firm-text">
     <div class="title-detail-firm">
-        <h2>${valuePresent.ten_phim}</h2>
+        <h2>${valuePresent['ten_phim']}</h2>
             <span>Till The End Of The Moon (2023)</span>
             <span>${valuePresent}</span>
     </div>
@@ -66,14 +71,14 @@ showMoreDetailFirm.innerHTML = `
             <p>Trạng thái:</p>
             <span>${valuePresent}</span>
         </div>
-        <p>Thời lượng: ${valuePresent.thoi_luong} / tập</p>
-            <p>Số tập: ${valuePresent.so_tap}</p>
+        <p>Thời lượng: ${valuePresent['thoi_luong']} / tập</p>
+            <p>Số tập: ${valuePresent['so_tap']}</p>
             <p>Tình trạng: Hoàn tất</p>
             <p>Ngôn ngữ: Phụ đề Việt</p>
-            <p>Năm sản xuất: ${valuePresent.nam_san_xuat}</p>
-            <p>Quốc gia: ${valuePresent.quoc_gia}</p>
-            <p>Thể loại: <a href="#">${valuePresent.the_loai}</a></p>
-        <p>Diễn viên: <a href="#">${valuePresent.dien_vien}</a></p>
+            <p>Năm sản xuất: ${valuePresent['nam_san_xuat']}</p>
+            <p>Quốc gia: ${valuePresent['quoc_gia']}</p>
+            <p>Thể loại: <a href="#">${valuePresent['the_loai']}</a></p>
+        <p>Diễn viên: <a href="#">${valuePresent['dien_vien']}</a></p>
     </div>
     <div class="feedback-firm">
         <div class="action-feedback-firm">
@@ -94,3 +99,21 @@ showMoreDetailFirm.innerHTML = `
 </div>
 </div>
 `
+
+
+window.onload = function(){
+    let user_login = localStorage.getItem('user_login');
+    if(user_login){
+        document.getElementById('login').remove();
+        document.getElementById('register').remove();
+        // var htmlObj = document.getElementsByClassName('action-header')[0];
+        // htmlObj.innerHTML = htmlObj.innerHTML + `<a href="#"><i class="fa-regular fa-user">         Hello, Bro!</i></a>`;
+
+        let $profile = document.createElement('a');
+        $profile.href = "#";
+        $profile.innerHTML = `<i class="fa-regular fa-user">         Hello, Bro!</i>`;
+
+        let $bookMark = document.getElementsByClassName('bookmark-user')[0];
+        $bookMark.before($profile);
+    }
+}
