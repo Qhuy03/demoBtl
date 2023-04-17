e72b65b4d30b40f8115ffe73b94cdba8368d1a59
Qhuy03 <huyntq2003@gmail.com>
2023-04-17 21:02:57 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,25 @@
 // })
 // console.log(ulShow);
 
+window.onload = function(){
+    let user_login = localStorage.getItem('user_login');
+    if(user_login){
+        document.getElementById('login').remove();
+        document.getElementById('register').remove();
+        // var htmlObj = document.getElementsByClassName('action-header')[0];
+        // htmlObj.innerHTML = htmlObj.innerHTML + `<a href="#"><i class="fa-regular fa-user">         Hello, Bro!</i></a>`;
+
+        let $profile = document.createElement('a');
+        $profile.href = "#";
+        $profile.innerHTML = `<i class="fa-regular fa-user">         Hello, Bro!</i>`;
+
+        let $bookMark = document.getElementsByClassName('bookmark-user')[0];
+        $bookMark.before($profile);
+    }
+}
+
+
+
 
 // show-Menu-mobile
         let menuEle = document.querySelector("#menu");
